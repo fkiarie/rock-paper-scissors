3fd58cb89008f7a49fe8bd9ef081347045ed1133
fkiarie <kazungu61@outlook.com>
2023-02-24 21:44:12 +0700

    Created computer selection and playground function
    The computer selection function selects either Rock, paper or
    Scissors using Math Rnadom. The variable is then passed to the
    playground function to play agianst the user.

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 console.log("Welcome to the game")
+
+function getComputerChoice(){
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choice.length);
+    const computerchoice = choice[randomIndex];
+    return computerchoice;
+
+}
+
+function playGround(userSelection, computerSelection){
+    //Check for a tie
+    if(userSelection == computerSelection){
+        return `You both selected ${computerSelection}. It is a tie`
+    }else if( userSelection == 'Rock'){  // check between Rock and Scissors
+        if(computerSelection == 'Scissors'){
+            return `${userSelection} smashes ${computerSelection} you win!`;
+        } else {
+            return `Paper covers ${userSelection}, You lose!`;
+        }
+    }else if( userSelection == 'Paper'){ // check between Paper and Rock
+        if(computerSelection == 'Rocks'){
+            return `${userSelection} covers ${computerSelection} you win!`;
+        } else {
+            return `Scissors cuts ${userSelection}, You lose!`;
+        }
+    }else if( userSelection == 'Scissors'){ // check between Scissor and Paper
+        if(computerSelection == 'Paper'){
+            return `${userSelection} cuts ${computerSelection} you win!`;
+        } else {
+            return `Rock smashes ${userSelection}, You lose!`;
+        }
+    }
+}
+
+const playerSelection = 'Scissors';
+const computerSelection = getComputerChoice();
+console.log(`You selected ${playerSelection} and the computer selected ${computerSelection}`);
+console.log(playGround(playerSelection,computerSelection));
