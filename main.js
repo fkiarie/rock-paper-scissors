bde8f342c34c8b2c3ddea9c62366cf39ebecaffc
fkiarie <kazungu61@outlook.com>
2023-03-29 14:54:36 +0700

    Add event listeners
    Added event listeners to the buttons such that they input the user
    selection.

--- main.js
+++ main.js
@@ -7,72 +7,52 @@ function getComputerChoice(){
     return computerchoice;
 }
 
-function userInput(){
-    const validInputs = ['rock' , 'paper' ,'scissors'];
-    const input = prompt("Enter: Rock | Paper | Scissors").toLowerCase();
 
-    if(!validInputs.includes(input)){
-        return `an invalid input: ${input}. Please enter rock, paper or scissors`;
-    }else{
-        return input
-    }
+function userInputButtons(){
+    const rockButton = document.getElementById("rock");
+    const paperButton = document.getElementById("paper");
+    const scissorsButton = document.getElementById("scissors");
+    
+
+    rockButton.addEventListener("click", function(){
+        const playerSelection = 'rock';
+        const computerSelection = getComputerChoice();
+        playGround(playerSelection,computerSelection);
+    });
+    paperButton.addEventListener("click", function(){
+        const playerSelection = 'paper';
+        const computerSelection = getComputerChoice();
+        playGround(playerSelection,computerSelection);
+    });
+    scissorsButton.addEventListener("click", function(){
+        const playerSelection = 'scissors';
+        const computerSelection = getComputerChoice();
+        playGround(playerSelection,computerSelection);
+    });
 }
-
 function playGround(userSelection, computerSelection){
     //Check for a tie
     if(userSelection == computerSelection){
-        return `You both selected ${computerSelection}. It is a tie`
+        console.log(`You both selected ${computerSelection}. It is a tie`);
     }else if( userSelection == 'rock'){  // check between Rock and Scissors
         if(computerSelection == 'scissors'){
-            return `${userSelection} smashes ${computerSelection} you win!`;
+            console.log(`${userSelection} smashes ${computerSelection} you win!`);
         } else {
-            return `Paper covers ${userSelection}, You lose!`;
+            console.log(`Paper covers ${userSelection}, You lose!`);
         }
     }else if( userSelection == 'paper'){ // check between Paper and Rock
         if(computerSelection == 'rock'){
-            return `${userSelection} covers ${computerSelection} you win!`;
+            console.log(`${userSelection} covers ${computerSelection} you win!`);
         } else {
-            return `Scissors cuts ${userSelection}, You lose!`;
+            console.log(`Scissors cuts ${userSelection}, You lose!`);
         }
     }else if( userSelection == 'scissors'){ // check between Scissor and Paper
         if(computerSelection == 'paper'){
-            return `${userSelection} cuts ${computerSelection} you win!`;
+            console.log(`${userSelection} cuts ${computerSelection} you win!`);
         } else {
-            return `Rock smashes ${userSelection}, You lose!`;
+            console.log(`Rock smashes ${userSelection}, You lose!`);
         }
     }
 }
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-  
-    for (let i = 1; i <= 5; i++) {
-      const playerSelection = userInput();
-      const computerSelection = getComputerChoice();
-      const roundResult = playGround(playerSelection, computerSelection);
-  
-      console.log(`Round ${i}: You selected ${playerSelection} and the computer selected ${computerSelection}. ${roundResult}`);
-  
-      if (roundResult.includes('win')) {
-        playerScore++;
-      } else if (roundResult.includes('lose')) {
-        computerScore++;
-      }
-    }
-  
-    console.log(`Game over! Your score: ${playerScore}. Computer score: ${computerScore}.`);
-  
-    if (playerScore > computerScore) {
-      console.log("Congratulations! You won the game!");
-    } else if (playerScore < computerScore) {
-      console.log("Sorry, you lost the game. Better luck next time!");
-    } else {
-      console.log("The game is a tie!");
-    }
-  }
-  
 
-// const playerSelection = userInput();
-// const computerSelection = getComputerChoice();
-// console.log(`You selected ${playerSelection} and the computer selected ${computerSelection}`);
-game();
+userInputButtons();
